14f51932b744fe8d9109e1766567431a21866309
Nao Takegawa <nao.takegawa316@gmail.com>
2018-10-09 21:54:40 +0900

    set up api, using restframework

---
+++ notes/api.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets, permissions
+
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteViewSet(viewsets.ModelViewSet):
+  queryset = Note.objects.all()
+  permission_classes = [permissions.AllowAny,]
+  serializer_class = NoteSerializer
---
+++ notes/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+  text = models.CharField(max_length=255)
+  created_at=models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.text
